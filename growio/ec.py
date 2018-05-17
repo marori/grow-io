cef291ce6dbeceb7d26cd4fb83c64af3f303d36e
Jorge Dias <mar.ori@gmail.com>
2018-05-17 01:14:19 +0000

    cleanup, cli argument parsing

--- growio/ec.py
+++ growio/ec.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 from dac import DAC8591
 from temp import Therm
 from liteio import LiteIO
@@ -15,19 +16,19 @@ class InvalidParameter(EcMeterException):
 class EcMeter(object):
     """This class reads the Electric Conductivity
 
-   To measure the EC, we use a US power plug, and a DS18B20 temperature sensor
+   To measure the EC, we use a US power plug as probe, and a DS18B20 temperature sensor
    Two GPIO pins (S1, S2) are used to control power sources to simulate alternate current
    A DAC8591 is used to measure the voltage on the power plug
    A Thermometer is needed to adjust the measurement for temperature
 
              Ri               Ra
-   S1 _____/\/\/\___________/\/\/\______
-                                        |            _______
-                                        |           |       |
-                                        |____ Vo ___|  ADC  |____
-                           o Plug o     |           |_______|
-             Ri            |      |     |
-   S2 _____/\/\/\__________|  Rw  |_____|
+   S1 _____/\/\/\___________/\/\/\_______
+                                         |            _______
+                                         |           |       |
+                                         |____ Vo ___|  ADC  |____
+                           o Probe o     |           |_______|
+             Ri            |       |     |
+   S2 _____/\/\/\__________|   Rw  |_____|
 
    Where S1 and S2 are GPIO Outputs, to be used as 3.3V alternating input source (S2 = !S1)
    Ri are the internal OrangePi resistor, which we can't change.
@@ -70,7 +71,7 @@ class EcMeter(object):
 
         :return: EC measurement results in siemens
         """
-        tc = EcMeter.temp_compensation
+        tc = EcMeter.temperature_compensation
         temp = self.temp.read()
         self.s1.write(0)
         self.s2.write(1)
@@ -79,17 +80,33 @@ class EcMeter(object):
         self.s2.write(0)
         v2 = self.adc.read(self.adc_port)
         self.s1.write(0)
-        print [v1, v2]
-        v = sum(v1, 255 - v2) / 2
+
+        #print([v1, v2])
+        v = sum([255 - v1, v2]) / 2
+        print v
+        print "ec25 = v * self.calibration / (1 + tc*(temp-25.0)) "
         ec25 = v * self.calibration / (1 + tc*(temp-25.0))
+        print "%s = %s * %s / (1 + %s * (%s - 25.0))" % (
+            ec25, v, self.calibration, tc, temp
+        )
         return ec25
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Read the EC of water.")
-    parser.add_argument("pins1")
-    parser.add_argument("pins2")
-    parser.add_argument("bus")
-    parser.add_argument("addr")
-    parser.add_argument("port")
-    parser.add_argument("device_id")
+    parser.add_argument("S1", metavar="S1", type=int, help="Input source board pin 1")
+    parser.add_argument("S2", metavar="S2", type=int, help="Input source board pin 2")
+    parser.add_argument("bus", metavar="I2CBUS", type=int, help="I2C bus - use i2cdetect")
+    parser.add_argument("addr", metavar="I2CADDRESS", type=str, help="I2C ADC address")
+    parser.add_argument("port", metavar="PORT", type=int, help="Analog port")
+    parser.add_argument("device_id", metavar="DEVICE_ID", type=str, help="Temperature sensor device ID")
+
+    args = parser.parse_args()
+    try:
+        address = int(args.addr)
+    except ValueError:
+        try:
+            address = int(args.addr, 16)
+        except ValueError:
+            print("The address must be an integer.")
+            sys.exit(1)
